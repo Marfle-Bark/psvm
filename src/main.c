75f5bedb475855fbc0997b9cef69b48b5cd5c3d5
Marfle-Bark <ethan.busbee@gmail.com>
2015-05-12 03:15:45 -0500

    Refactored a fair bit of code from main.c into main.h. Updated the makefile. Made notes.

--- src/main.c
+++ src/main.c
@@ -3,22 +3,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-
-//program instructions
-typedef enum {
-  PUSH,  //PUSH X: Push DWORD X onto the stack.
-  POP,  //POP: Pop top DWORD off the stack.
-  ADD,  //ADD: Pop top two DWORDs off of the stack, add them, push result.
-  SUB,  //SUB: Pop top two DWORDS off the stack, subtract them, push result.
-  MUL, //MUL: Pop two DWORDS off the stack, multiply them, push result.
-  DIV,  //DIV: Pop two DWORDS off the stack, divide them, push result.
-  PEEK, //PEEK: Peek top DWORD off the stack and print.
-  HALT   //HALT: End program execution.
-} InstructionSet;
-
-typedef unsigned char BYTE;
-typedef unsigned short WORD;
-typedef unsigned long DWORD;
+#include "main.h"
 
 int ip = 0;           //instruction pointer
 int sp = -1;          //stack pointer
@@ -129,7 +114,7 @@ void run(DWORD program[], bool enableDebug) {
   dumpStack();
 }
 
-int main() {
+void test() {
   DWORD test[] = {
     PUSH, 1,
     PUSH, 2,
@@ -148,8 +133,6 @@ int main() {
   };
 
   run(test, true);
-
-  return 0;
 }
 
 #endif
